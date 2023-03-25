dd14798f6d18b32300df76bd879c297bdfada1db
Jeff Biggus <hyperjeff@mac.com>
2023-03-25 16:10:48 -0500

    Added watchOS support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Physical", platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13)],
+	name: "Physical", platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v4)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
