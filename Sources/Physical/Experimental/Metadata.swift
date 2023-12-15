c3b0fb242aa18562abb0eadc9d961d8897c1f382
Jeff Biggus <hyperjeff@mac.com>
2023-12-15 14:46:49 -0600

    Added decibels and differences to KindOfQuantity

--- Sources/Physical/Experimental/Metadata.swift
+++ Sources/Physical/Experimental/Metadata.swift
@@ -1,7 +1,26 @@
-public struct Metadata {
-	public var referenceInfo: [String] = []
+import Foundation
+
+public indirect enum KindOfQuantity {
+	case ratio(denominator: Physical)
+	case decibel(reference: Physical, symbol: String)
+	case standardDecibel(reference: DBReference)
+	case difference
 }
 
-public enum KindOfQuantity {
-	case ratio, energy, torque, heatCapacity, entropy // list could be infinite
+extension Physical {
+	public var isDifference: Bool {
+		if let kind = kindOfQuantity,
+		   case .difference = kind {
+			return true
+		}
+		
+		return false
+	}
+	
+	public var difference: Physical {
+		var out = self
+		out.kindOfQuantity = .difference
+		
+		return out
+	}
 }
